dab6bca38b14296e940d03851ed24a23166dcf1a
Vasif Sultanov <cdm_89@mail.ru>
2024-07-11 16:29:16 +0500

    Mux

--- Tipsy-iOS13/Tipsy/Controller/CalculateViewController.swift
+++ Tipsy-iOS13/Tipsy/Controller/CalculateViewController.swift
@@ -21,6 +21,7 @@ class CalculateViewController: UIViewController, UITextFieldDelegate {
     var splitCount = 2
     var result = ResultModel()
     var textFields: [UITextField] = []
+    var textLabels: [UILabel] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,19 +57,30 @@ class CalculateViewController: UIViewController, UITextFieldDelegate {
     func splitingAndTextFields() {
         // Удаление всех предыдущих текстовых полей
         textFields.forEach { $0.removeFromSuperview() }
+        textLabels.forEach { $0.removeFromSuperview() }
         textFields.removeAll()
+        textLabels.removeAll()
 
         // Получение количества людей из inputBillSum
         if splitCount != 0, splitCount > 0 {
             var previousTextField: UITextField?
 
             // Добавление текстовых полей в UIScrollView
-            for _ in 0 ..< splitCount {
+            for e in 0 ..< splitCount {
+                
+                let textLabel = UILabel()
+                textLabel.text = "\(e+1)."
+                textLabel.textColor = .lightGray
+                peoples.addSubview(textLabel)
+                textLabels.append(textLabel)
+               
+                
                 let textField = UITextField()
                 textField.borderStyle = .roundedRect
                 textField.placeholder = "Введите значение"
                 peoples.addSubview(textField)
                 textFields.append(textField)
+               
 
                 // Установка ограничений для каждого текстового поля
                 textField.translatesAutoresizingMaskIntoConstraints = false
@@ -82,7 +94,24 @@ class CalculateViewController: UIViewController, UITextFieldDelegate {
                     textField.topAnchor.constraint(equalTo: peoples.topAnchor, constant: 20).isActive = true
                 }
 
+                // Установка ограничений для каждого текстового поля
+                textLabel.translatesAutoresizingMaskIntoConstraints = false
+                
+                textLabel.leadingAnchor.constraint(equalTo: peoples.leadingAnchor, constant: 0).isActive = true
+                
+                textLabel.trailingAnchor.constraint(equalTo: peoples.trailingAnchor, constant: -20).isActive = true
+                
+                textLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+
+                if let previousTextField = previousTextField {
+                    textLabel.topAnchor.constraint(equalTo: previousTextField.bottomAnchor, constant: 20).isActive = true
+                } else {
+                    textLabel.topAnchor.constraint(equalTo: peoples.topAnchor, constant: 20).isActive = true
+                }
+
+                
                 previousTextField = textField
+                
             }
 
             // Установка ограничений для последнего текстового поля
@@ -91,33 +120,41 @@ class CalculateViewController: UIViewController, UITextFieldDelegate {
             }
         }
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // Разрешаем только цифры и одну десятичную точку
+        let allowedCharacters = CharacterSet(charactersIn: "0123456789.")
+        let characterSet = CharacterSet(charactersIn: string)
+
+        // Проверяем, что вводимые символы разрешены
+        if !allowedCharacters.isSuperset(of: characterSet) && !(string == ".") {
+            return false
+        }
+
+        // Проверяем, что уже введенная строка содержит не более одной десятичной точки
+        let currentText = textField.text ?? ""
+        let newString = (currentText as NSString).replacingCharacters(in: range, with: string)
+
+        // Ограничиваем длину текста до 15 символов
+        let maxLength = 15
+        if newString.count > maxLength {
+            return false
+        }
+
+        // Проверяем, что в строке не больше одной десятичной точки
+        let components = newString.components(separatedBy: ".")
+        if components.count > 2 {
+            return false
+        }
+
+        return true
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
-func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-    // Разрешаем только цифры и одну десятичную точку
-    let allowedCharacters = CharacterSet(charactersIn: "0123456789.")
-    let characterSet = CharacterSet(charactersIn: string)
-
-    // Проверяем, что вводимые символы разрешены
-    if !allowedCharacters.isSuperset(of: characterSet) && !(string == ".") {
-        return false
-    }
-
-    // Проверяем, что уже введенная строка содержит не более одной десятичной точки
-    let currentText = textField.text ?? ""
-    let newString = (currentText as NSString).replacingCharacters(in: range, with: string)
-
-    // Ограничиваем длину текста до 15 символов
-    let maxLength = 15
-    if newString.count > maxLength {
-        return false
-    }
-
-    // Проверяем, что в строке не больше одной десятичной точки
-    let components = newString.components(separatedBy: ".")
-    if components.count > 2 {
-        return false
-    }
-
-    return true
-}
