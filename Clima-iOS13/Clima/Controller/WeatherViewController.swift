f6de336ddfd907d2b44280d4fb7f60ce17c38a8a
Vasif <sultanovasif@gmail.com>
2024-09-24 07:53:50 +0500

    24.09.2024

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -104,6 +104,7 @@ extension WeatherViewController: UITextFieldDelegate {
 // MARK: - WeatherManagerDelegate
 
 extension WeatherViewController: WeatherManagerDelegate {
+    
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
         DispatchQueue.main.async {
             self.temperatureLabel.text = weather.temperatureString
